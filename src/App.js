a9426b6e2f75eb9031784f3e4a6149d7639ef0f4
rocionadal <rocioonadal@gmail.com>
2021-02-20 13:54:43 +0100

    props and state

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Rocio', age: 21},
+      { name: 'Rocio', age: 21 },
+      { name: 'Felipe', age: 22 },
+      { name: 'Pilar', age: 19 }
     ]
   }
 
@@ -15,9 +17,9 @@ class App extends Component {
        <h1>Hi, I'm a React App</h1>
        <p>This is really working!</p>
        <button>Switch Name</button>
-       <Person name="Rocio" age="21" />
-       <Person name="Felipe" age="22" >My Hobbies: Racing</Person>
-       <Person name="Pilar" age="19"  />
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Racing</Person>
+       <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }

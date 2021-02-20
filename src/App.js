ddd76177f9a64240c328a9317a2c28854fd07751
rocionadal <rocioonadal@gmail.com>
2021-02-20 14:34:11 +0100

    multiple useState hook

--- src/App.js
+++ src/App.js
@@ -1,40 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: 'Rocio', age: 21 },
       { name: 'Felipe', age: 22 },
       { name: 'Pilar', age: 19 }
     ]
-  }
+  });
 
-  switchNameHandler = () => {
+  const [otherState, setOtherState] = useState('some other value');
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
     // console.log('Was clicked!');
     // DON'T DO THIS this.state.persons[0].name = 'Rochi';
-    this.setState( { // allows to update state property
+    setPersonsState( { // allows to update state property
       persons: [
         { name: 'Rochi', age: 21 },
         { name: 'Felipe', age: 22 },
         { name: 'Pilar', age: 30 }
       ] 
-    } ) 
-  }
+    }); 
+  };
 
-  render() {
     return (
       <div className="App">
        <h1>Hi, I'm a React App</h1>
        <p>This is really working!</p>
-       <button onClick={this.switchNameHandler}>Switch Name</button>
-       <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-       <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Racing</Person>
-       <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+       <button onClick={switchNameHandler}>Switch Name</button>
+       <Person 
+        name={personsState.persons[0].name} 
+        age={personsState.persons[0].age} 
+      />
+       <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >
+        My Hobbies: Racing
+       </Person>
+       <Person ç
+        name={personsState.persons[2].name} 
+        age={personsState.persons[2].age}
+      />
       </div>
     );
-  }
 }
 
-export default App;
+export default app;
+
+
